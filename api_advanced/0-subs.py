af060ee39296cb408dbfc0e9b5915d0f9f3e996b
Jobealieu <a.jobe@alustudent.com>
2025-10-30 02:31:40 -0700

    Add 0-subs.py script and README

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,44 +1,17 @@
 #!/usr/bin/python3
-"""
-Module to query Reddit API for top 10 hot posts
-"""
+"""Return the number of subscribers of a given subreddit"""
+
 import requests
 
 
-def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
-
-    Args:
-        subreddit: The name of the subreddit to query
-
-    Returns:
-        None - prints titles or None if invalid subreddit
-    """
-    if subreddit is None or not isinstance(subreddit, str):
-        print(None)
-        return
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        'User-Agent': 'python:api_advanced:v1.0.0 (by /u/yourusername)'
-    }
-    params = {'limit': 10}
+def number_of_subscribers(subreddit):
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
     try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-
-            for post in posts:
-                title = post.get('data', {}).get('title')
-                print(title)
-        else:
-            print(None)
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
 
     except Exception:
-        print(None)
+        return 0
