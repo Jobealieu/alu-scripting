68bbcb7d772fea36ccf85dd7783a5faf39655daa
Jobealieu <a.jobe@alustudent.com>
2025-10-30 12:13:00 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,56 +1,16 @@
 #!/usr/bin/python3
-"""Module to query Reddit API for hot posts."""
-
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Print the titles of the first 10 hot posts for a given subreddit.
-
-    Args:
-        subreddit: The name of the subreddit to query
-
-    Returns:
-        None
-    """
-    if subreddit is None or not isinstance(subreddit, str):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
         return
-
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        'User-Agent': 'Mozilla/5.0'
-    }
-    params = {'limit': 10}
-
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            params=params,
-            allow_redirects=False
-        )
-
-        if response.status_code == 200:
-            data = response.json()
-            
-            # Check if we have valid data structure
-            if 'data' not in data or 'children' not in data.get('data', {}):
-                print(None)
-                return
-            
-            children = data['data']['children']
-            
-            if len(children) == 0:
-                print(None)
-                return
-
-            for child in children:
-                if 'data' in child and 'title' in child['data']:
-                    print(child['data']['title'])
-        else:
-            print(None)
-
-    except Exception:
-        print(None)
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
