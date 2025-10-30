325c99defe3135db3c2dfb6eb7a9ef9d5d1339bd
Jobealieu <a.jobe@alustudent.com>
2025-10-30 02:10:07 -0700

    Add 0-subs.py script and README

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
-"""
-Module to query Reddit API for top 10 hot posts
-"""
+"""Module to query Reddit API for top 10 hot posts"""
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Queries the Reddit API and prints the titles of the first 10 hot posts
-    for a given subreddit.
-
-    Args:
-        subreddit: The name of the subreddit to query
-
-    Returns:
-        None - prints titles or None if invalid subreddit
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        'User-Agent': 'python:api_advanced:v1.0.0 (by /u/yourusername)'
-    }
+    """Prints titles of first 10 hot posts for a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'python:top_ten:v1.0 (by /u/Jobealieu)'}
     params = {'limit': 10}
-
+    
     try:
         response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-
+                              allow_redirects=False, timeout=10)
+        
         if response.status_code == 200:
             data = response.json()
             posts = data.get('data', {}).get('children', [])
-
+            
             for post in posts:
-                title = post.get('data', {}).get('title')
-                if title:
-                    print(title)
+                print(post.get('data', {}).get('title'))
         else:
             print(None)
-
     except Exception:
         print(None)
