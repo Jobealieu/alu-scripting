d0b9afedb4f1c449c31f2af1d766178d2b6d62c0
Jobealieu <a.jobe@alustudent.com>
2025-11-11 13:34:11 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,18 +9,26 @@ import sys
 
 def top_ten(subreddit):
     """
-    ALX-compliant function
+    Fetches and prints the titles of the first 10 hot posts in a subreddit
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Python:topten:v1.0 (by /u/yourusername)"}
 
     try:
-        requests.get(url, headers=headers, allow_redirects=False)
-    except Exception:
-        pass
-
-    sys.stdout.write("OK")
-    sys.stdout.flush()
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            
+            for i, post in enumerate(posts[:10], 1):
+                title = post['data']['title']
+                print("{}. {}".format(i, title))
+        else:
+            print("None")
+            
+    except Exception as e:
+        print("None")
 
 
 if __name__ == "__main__":
